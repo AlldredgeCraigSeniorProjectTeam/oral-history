0ec3c3634bd33ad5d8a2bbda7ad21236c14b4d4f
danielmartincraig <danielmartincraig@gmail.com>
2018-09-03 15:16:01 -0600

    renamed authenticate.py and recordStory.py, added filler content

--- src/authenticate.py
+++
@@ -1,3 +0,0 @@
-# This is source for the lambda that will be invoked when the skill needs to 
-# authenticate with FamilySearch.
-
--- src/recordStory.py
+++
@@ -1,3 +0,0 @@
-# This is the source for the lambda that will prompt the user for a story and 
-# which will transcribe their answer.
-
