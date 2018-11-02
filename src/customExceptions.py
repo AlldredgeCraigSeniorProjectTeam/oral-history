04276e051936255b53c52cf9cdebcf6eebd433b0
Daniel Craig <danielmartincraig@gmail.com>
2018-11-02 00:24:35 -0600

    Fixed it so that it will retrieve the full text of stories, and so that it won't try to grab a memory that isn't plaintext

--- src/customExceptions.py
+++ src/customExceptions.py
@@ -4,4 +4,4 @@ class httpError401Exception(Exception):
 
 class httpErrorUnhandledException(Exception):
     def __init__(self, errorCode):
-        Exception.__init__(self, "The call to the FamilySearch API returned a " + errorCode + " error")
+        Exception.__init__(self, "The call to the FamilySearch API returned a " + str(errorCode) + " error")
--- src/familySearchAPIDecorator.py
+++ src/familySearchAPIDecorator.py
@@ -1,6 +1,6 @@
 import requests
 import random
-from xml.etree import ElementTree
+import json
 from customExceptions import httpError401Exception, httpErrorUnhandledException
 
 
@@ -24,18 +24,24 @@ class FSDecorator:
             access_token = self.session['user']['accessToken']           
 
             headers = {
-                'Accept': "application/x-fs-v1+xml",
+                'Accept': "application/json",
                 'Authorization': "Bearer " + access_token,
                 }
 
             response = requests.request("GET", url, headers=headers)
 
             if response.status_code == 200:
-                root = ElementTree.fromstring(response.text)
-                memoryElementList = root.findall("{http://gedcomx.org/v1/}sourceDescription")
-                memoryIdList = [memoryElement.attrib['id'] for memoryElement in memoryElementList]
-                randomMemoryID = memoryIdList[random.randint(0, len(memoryIdList) - 1)]
-                return randomMemoryID
+                responseData = json.loads(response.text)
+                listOfMemories = responseData['sourceDescriptions']
+                listOfMemoryIDs = [memory['id'] for memory in listOfMemories if memory['mediaType'] == 'text/plain']
+
+                if len(listOfMemoryIDs) >= 1:
+                    randomMemoryID = listOfMemoryIDs[random.randint(0, len(listOfMemoryIDs) - 1)]
+                    return randomMemoryID
+                else:
+                    # Return a negative 1 if there are no text memories available
+                    return -1
+
             elif response.status_code == 401:
                 # You need to reauthenticate
                 raise httpError401Exception()
@@ -50,35 +56,37 @@ class FSDecorator:
             access_token = self.session['user']['accessToken']
 
             headers = {
-                'Accept': "application/x-fs-v1+xml",
+                'Accept': "application/json",
                 'Authorization': "Bearer " + access_token,
                 }
 
             # If this raises a 401 exception, we want it to bubble up to the calling function
             id = self.getRandomMemoryID()
 
+            # getRandomMemory returns -1 if there are no memories available to listen to.
+            if  id == -1:
+                speech_output = "There are no memories available.  Please try recording a new memory so that you'll have something to listen to."
+                return speech_output
+            
+            # We passed the -1 check, so assume that the id is good. 
             response = requests.request("GET", url+id, headers=headers)
 
             response_status_code = response.status_code
             print("The request returned a status code of " + str(response_status_code))
 
-            #######################################################################################
-            # NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE 
-            # This logic assumes that all the memories are text memories -note that this is invalid
-            #######################################################################################
-
             if response.status_code == 200:
-                root = ElementTree.fromstring(response.text)
-                stories = root.findall(".//{http://gedcomx.org/v1/}description")
-
-                speech_output = stories[0].text
-                return speech_output
+                responseJsonObj = json.loads(response.text)
+                urlOfMemoryText = responseJsonObj['sourceDescriptions'][0]['about']
             elif response.status_code == 401:
                 # You need to reauthenticate
                 raise httpError401Exception()
             else:
                 # Unhandled status code
                 raise httpErrorUnhandledException(response_status_code)
+        
+            memoryTextResponse = requests.request("GET", urlOfMemoryText, headers=headers)
+            memoryText = memoryTextResponse.text
+            return memoryText
 
     instance = None
     def __init__(self, session):
