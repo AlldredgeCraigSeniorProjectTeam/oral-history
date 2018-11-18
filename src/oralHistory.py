671066ac00067de056427bdcab4daa3f47fe4aa0
Daniel Craig <danielmartincraig@gmail.com>
2018-11-17 20:39:32 -0700

    Rewrote oralHistory to use decorator pattern with ASK generated card responses

--- src/oralHistory.py
+++ src/oralHistory.py
@@ -1,18 +1,16 @@
 from __future__ import print_function
 from random import randint as rand
 
-import os
-import requests
-import uuid
+import os, requests, logging
 
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
-from ask_sdk_core.utils import is_intent_name
-from ask_sdk_core.response_helper import get_plain_text_content
 
 from familySearchAPIDecorator import FSDecorator
 from customExceptions import httpError401Exception, httpError403Exception, httpErrorUnhandledException
 
+<<<<<<< HEAD
 import boto3
 from botocore.client import Config
 
@@ -75,49 +73,59 @@ def get_welcome_response():
     """ If we wanted to initialize the session to have some attributes we could
     add those here
     """
+=======
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model.ui import LinkAccountCard
+from ask_sdk_model import Response
+>>>>>>> de99e8c... Rewrote oralHistory to use decorator pattern with ASK generated card responses
 
-    session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "Welcome to Family History! Try saying 'tell me a story'"
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
-    reprompt_text = ""
+sb = SkillBuilder()
 
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
-
-def get_link_account_response():
-    session_attributes = {}
-    return build_response(session_attributes, build_link_account_response())
-
-def get_reauthenticate_response():
-    session_attributes = {}
-    return build_response(session_attributes, build_reauthenticate_response())
-
-def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = 'Goodbye!'
-    # Setting this to true ends the session and exits the skill.
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
-# --------------- Events ------------------
+@sb.request_handler(can_handle_func=is_intent_name("record_history"))
+def record_history_intent_handler(handler_input):
+    """ Grab raw user text from AMAZON.custom_slot and write it to generic file
+       connect to s3 bucket using env variables in lambda and push it to
+       AWS using a unique name to prevent overwrite.
+       Additionally attempts to push story to FS as a memory. """
 
-def on_session_started(session_started_request, session):
-    """ Called when the session starts """
+    # Grab the session
+    session = handler_input.request_envelope.session
 
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
+    # Grab the intent
+    intent = handler_input.request_envelope.request.intent
 
-def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
-    """
+    # Grab the slots
+    story = intent['slots']['story']['value']
 
-    print("on_launch requestId=" + launch_request['requestId'] + ", sessionId=" + session['sessionId'])
+    FS = FSDecorator(session).getInstance()
 
+    try:
+        speech_text = FS.postMemory(story)
+    except httpError401Exception, e:
+        # This is where we reauthenticate because we got a 401 response.
+        speech_text = "Your session has expired.  Please proceed to the Alexa app to sign in again using the Link Account button."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
+    except httpError403Exception, e:
+        # This is where we reauthenticate because we got a 403 response.
+        speech_text = "Your session has expired.  Please proceed to the Alexa app to sign in again using the Link Account button."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
+    # Once again, we are intentionally not catching httpErrorUnhandledException
+
+    # If there are no exceptions, read the response back to the user.
+    return handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Family History", speech_text)).set_should_end_session(
+        True).response
+
+
+<<<<<<< HEAD
     try:
         # Try getting the access token, as a test of whether the user has linked the account
         access_token = session['user']['accessToken']
@@ -166,88 +174,123 @@ def record_history(intent, session):
 
 ##############################################
 # This is what it will look like to use the FSDecorator.
+=======
+@sb.request_handler(can_handle_func=is_intent_name("read_history"))
+def read_history_intent_handler(handler_input):
+    """ Read a story from FamilySearch """
+    session = handler_input.request_envelope.session
+
+>>>>>>> de99e8c... Rewrote oralHistory to use decorator pattern with ASK generated card responses
     FS = FSDecorator(session).getInstance()
 
     try:
-        speech_output = FS.postMemory(story)
+        speech_text = FS.getMemory()
     except httpError401Exception, e:
-        # This is where you reauthenticate.
-        return get_reauthenticate_response()
+        # This is where we reauthenticate because we got a 401 response.
+        speech_text = "Your session has expired.  Please proceed to the Alexa app to sign in again using the Link Account button."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
     except httpError403Exception, e:
-        return get_reauthenticate_response()
+        # This is where we reauthenticate because we got a 403 response.
+        speech_text = "Your session has expired.  Please proceed to the Alexa app to sign in again using the Link Account button."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
+    # We are intentionally not catching httpErrorUnhandledException
 
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-##############################################
+    # If there are no exceptions, read the story to the user.
+    return handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Family History", speech_text)).set_should_end_session(
+        True).response
 
+<<<<<<< HEAD
 def read_history(intent, session):
     '''Connect to FS, if successful retrieve a memory else attempt reauth'''
     session_attributes = {}
     reprompt_text = None
     card_title = "Read History"
     should_end_session = False
+=======
+>>>>>>> de99e8c... Rewrote oralHistory to use decorator pattern with ASK generated card responses
 
-    FS = FSDecorator(session).getInstance()
+@sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
+def launch_request_handler(handler_input):
+    """ Handler for the skill launch. Called when the user launches the skill without specifying what they want """
+
+    # Grab the session
+    session = handler_input.request_envelope.session
+
+    # Grab the request ID 
+    requestId = handler_input.request_envelope.request.requestId
+
+    print("on_launch requestId=" + requestId + ", sessionId=" + session['sessionId'])
 
     try:
-        speech_output = FS.getMemory()
-    except httpError401Exception, e:
-        # This is where you reauthenticate.
-        return get_reauthenticate_response()
-    except httpError403Exception, e:
-        return get_reauthenticate_response()
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+        # Try getting the access token, as a test of whether the user has linked the account
+        access_token = session['user']['accessToken']
+        
+        speech_text = "Welcome to Family History! Try saying 'tell me a story'"
 
-def on_intent(intent_request, session):
-    """ Called when the user specifies an intent for this skill """
+        return handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Family History", speech_text)).set_should_end_session(
+            False).response
+        
+    except KeyError, e:
+        speech_text = "Welcome to Family History! To get the most out of this Skill, please link your account."
 
-    print("on_intent requestId=" + intent_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-
-    intent = intent_request['intent']
-    intent_name = intent_request['intent']['name']
-
-    # Dispatch to your skill's intent handlers
-    if intent_name == "record_history":
-        return record_history(intent, session)
-    elif intent_name == "read_history":
-        return read_history(intent, session)
-    elif intent_name == "AMAZON.HelpIntent":
-        return get_welcome_response()
-    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-        return handle_session_end_request()
-    else:
-        raise ValueError("Invalid intent")
+        return handler_input.response_builder.speak().set_card(
+            SimpleCard("Family History", speech_text)).set_should_end_session(
+            True).response
 
 
-def on_session_ended(session_ended_request, session):
-    """ Called when the user ends the session.
+@sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
+def help_intent_handler(handler_input):
+    """Handler for Help Intent."""
+    speech_text = "Welcome to Family History! Try saying 'tell me a story'"
 
-    Is not called when the skill returns should_end_session=true
+    return handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Family History", speech_text)).response
+
+@sb.request_handler(
+    can_handle_func=lambda handler_input:
+        is_intent_name("AMAZON.CancelIntent")(handler_input) or
+        is_intent_name("AMAZON.StopIntent")(handler_input))
+def cancel_and_stop_intent_handler(handler_input):
+    """Single handler for Cancel and Stop Intent."""
+
+    speech_text = "Goodbye!  Come back soon!"
+
+    return handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Family History", speech_text)).response
+
+
+@sb.request_handler(can_handle_func=is_intent_name("AMAZON.FallbackIntent"))
+def fallback_handler(handler_input):
+    """ The fallback handler """
+    speech = (
+        "Try saying 'tell me a story'.")
+    reprompt = "I'm sorry, please try to speak more clearly!"
+    handler_input.response_builder.speak(speech).ask(reprompt)
+    return handler_input.response_builder.response
+
+
+@sb.request_handler(can_handle_func=is_request_type("SessionEndedRequest"))
+def session_ended_request_handler(handler_input):
+    """Handler for Session End."""
+    return handler_input.response_builder.response
+
+
+@sb.exception_handler(can_handle_func=lambda i, e: True)
+def all_exception_handler(handler_input, exception):
+    """Catch all exception handler, log exception and
+    respond with custom message.
     """
-    print("on_session_ended requestId=" + session_ended_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    # add cleanup logic here
+    logger.error(exception, exc_info=True)
 
+    speech = "There was a problem. Please try again!"
+    handler_input.response_builder.speak(speech).ask(speech)
 
-# --------------- Main handler ------------------
+    return handler_input.response_builder.response
 
-def lambda_handler(event, context):
-    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
-    etc.) The JSON body of the request is provided in the event parameter.
-    """
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
-
-    # This call is strictly for logging purposes
-    if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
-
-    if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
-    elif event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
-    elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+handler = sb.lambda_handler()
