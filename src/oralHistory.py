a46a80071e9e8a36544e7b4f6e9ddaa942e2834d
Daniel Craig <danielmartincraig@gmail.com>
2018-12-10 19:59:00 -0700

    Now pushes interview to FamilySearch

--- src/oralHistory.py
+++ src/oralHistory.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from random import randint as rand
 
-import os, requests, logging
+import os, requests, logging, datetime
 
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.utils import is_request_type, is_intent_name
@@ -133,20 +133,97 @@ def read_history_intent_handler(handler_input):
 @sb.request_handler(
     can_handle_func=lambda handler_input:
         is_intent_name("interview_me") and
-        handler_input.request_envelope.request.dialog_state.value != "COMPLETED")
+        handler_input.request_envelope.request.dialog_state.value == "STARTED")
+def before_starting_interview_me_intent_handler(handler_input):
+
+   # Grab the access token
+    access_token = handler_input.request_envelope.session.user.access_token
+    
+    FS = FSDecorator(access_token).getInstance()
+
+    try:
+        speech_text = FS.getMemory()
+    except httpError401Exception, e:
+        # This is where we reauthenticate because we got a 401 response.
+        speech_text = "Your session has expired.  Please proceed to the Alexa app to sign in again using the Link Account button."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
+    except httpError403Exception, e:
+        # This is where we reauthenticate because we got a 403 response.
+        speech_text = "Your session has expired.  Please proceed to the Alexa app to sign in again using the Link Account button."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
+    except TypeError, e:
+        # This is where we authenticate because a brand new user has no access token whatsoever, so he got a type error.
+        speech_text = "Welcome to Family History! To get the most out of this Skill, please link your account."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
+    # We are intentionally not catching httpErrorUnhandledException
+
+    my_delegate_directive = dialog.delegate_directive.DelegateDirective()
+    return handler_input.response_builder.add_directive(my_delegate_directive).response
+
+@sb.request_handler(
+    can_handle_func=lambda handler_input:
+        is_intent_name("interview_me") and
+        handler_input.request_envelope.request.dialog_state.value == "IN_PROGRESS")
 def in_progress_interview_me_intent_handler(handler_input):
     current_intent = handler_input.request_envelope.request.intent.name
     my_delegate_directive = dialog.delegate_directive.DelegateDirective()
 
     return handler_input.response_builder.add_directive(my_delegate_directive).response
-    
-@sb.request_handler(can_handle_func=lambda handler_input:
-        is_intent_name("interview_me"))
+
+@sb.request_handler(
+    can_handle_func=lambda handler_input:
+        is_intent_name("interview_me") and
+        handler_input.request_envelope.request.dialog_state.value == "COMPLETED")
 def completed_interview_me_intent_handler(handler_input):
     speech_text = "Thanks for the interview!"
 
+    # Grab the access token
+    access_token = handler_input.request_envelope.session.user.access_token
+    
+    interview_text = "Interview with Family History Skill, " + datetime.datetime.now().strftime("%A, %x") + ":\n"
+
+    slot_questions = {'grandmas_house_color': 'What color was your grandmother\'s house?',
+        'grandmas_favorite_game': 'What was your grandmother\'s favorite game?', 
+        'grandmas_favorite_dessert': 'What was your grandma\'s favorite dessert?'}
+
+    for slot in handler_input.request_envelope.request.intent.slots:
+        interview_text += "Family History: " 
+        interview_text += slot_questions[slot] + "\n"
+        interview_text += "User: "
+        interview_text += str(handler_input.request_envelope.request.intent.slots[slot].value + "\n\n")
+
+    FS = FSDecorator(access_token).getInstance()
+
+    try:
+        speech_text = FS.postMemory(interview_text)
+    except httpError401Exception, e:
+        # This is where we reauthenticate because we got a 401 response.
+        speech_text = "Your session has expired.  Please proceed to the Alexa app to sign in again using the Link Account button."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
+    except httpError403Exception, e:
+        # This is where we reauthenticate because we got a 403 response.
+        speech_text = "Your session has expired.  Please proceed to the Alexa app to sign in again using the Link Account button."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
+    except TypeError, e:
+        # This is where we authenticate because a brand new user has no access token whatsoever, so he got a type error.
+        speech_text = "Welcome to Family History! To get the most out of this Skill, please link your account."
+        return handler_input.response_builder.speak(speech_text).set_card(
+            LinkAccountCard()).set_should_end_session(
+            True).response
+    # Once again, we are intentionally not catching httpErrorUnhandledException
+
     return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Family History", speech_text)).response
+        SimpleCard("Family History", speech_text)).set_should_end_session(True).response
 
 @sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
 def help_intent_handler(handler_input):
@@ -171,8 +248,7 @@ def cancel_and_stop_intent_handler(handler_input):
 @sb.request_handler(can_handle_func=is_intent_name("AMAZON.FallbackIntent"))
 def fallback_handler(handler_input):
     """ The fallback handler """
-    speech = (
-        "Try saying 'tell me a story'.")
+    speech = ("Try saying 'tell me a story'.")
     reprompt = "I'm sorry, please try to speak more clearly!"
     handler_input.response_builder.speak(speech).ask(reprompt)
     return handler_input.response_builder.response
